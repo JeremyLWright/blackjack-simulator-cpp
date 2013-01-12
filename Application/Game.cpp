dea89c130e6de2f992b0852f510e4714bc20111c
Jeremy Wright <jeremy@codestrokes.com>
2013-01-12 13:29:08 -0700

    Added example Game Class.

---
+++ Application/Game.cpp
@@ -0,0 +1,11 @@
+#include "Game.hpp"
+namespace Application {
+    Game::Game ()
+    {
+    }
+
+    virtual ~Game ()
+    {
+    }
+
+} /* Application */
---
+++ Application/Game.hpp
@@ -0,0 +1,16 @@
+#ifndef _GAME
+#define _GAME
+namespace Application {
+    class Game {
+        public:
+            Game ();
+            virtual ~Game ();
+
+        private:
+            /* data */
+    };
+
+} /* Application */
+
+
+#endif /* end of include guard: _GAME */
